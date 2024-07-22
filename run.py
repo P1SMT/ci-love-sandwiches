78d90d456bd0f70cf15ce4290aed1cae30ccbb19
Steve Taylor <steve@p1smt.com>
2024-07-22 15:41:20 +0000

    change order of code so all functions defined before any are called

--- run.py
+++ run.py
@@ -145,13 +145,13 @@ def main():
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, "stock")
 
-print("Welcome to Love Sandwiches Data Automation:\n")
-stock_data = main() #change to include Love Sandwiches Challange
-
 def get_stock_values(data):
     headings = SHEET.worksheet("stock").get_all_values()[0]
     stockDict = { headings:data for (headings,data) in zip(headings, data)}
     return stockDict
+
+print("Welcome to Love Sandwiches Data Automation:\n")
+stock_data = main() #change to include Love Sandwiches Challange
     
 stock_values = get_stock_values(stock_data)
 
